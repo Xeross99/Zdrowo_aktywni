14d40a61dfed5ba9980f84f22b129eaa7d75cd3f
Michał Krzysteczko <m.krzysteczko@icloud.com>
2025-01-11 02:07:42 +0100

    added mail icon to Icons, added target blank to links

--- src/app/o-mnie/page.jsx
+++ src/app/o-mnie/page.jsx
@@ -3,31 +3,17 @@ import Link from 'next/link'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import { InstagramIcon, FacebookIcon, StravaIcon, TikTokIcon } from '@/components/SocialIcons'
+import { InstagramIcon, FacebookIcon, StravaIcon, TikTokIcon, MailIcon } from '@/components/Icons'
 import portraitImage from '@/images/sport/img3.jpg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
     <li className={clsx(className, 'flex')}>
-      <Link
-        href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-lime-500"
-      >
+      <Link href={href} target="_blank" className="group flex text-sm font-medium text-zinc-800 transition hover:text-lime-500">
         <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-lime-500" />
         <span className="ml-4">{children}</span>
       </Link>
     </li>
-  )
-}
-
-function MailIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
-      />
-    </svg>
   )
 }
 
