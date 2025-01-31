61ceedbfe461bac8f44b332a894144e7ffa07767
Michał Krzysteczko <m.krzysteczko@icloud.com>
2025-01-31 22:22:51 +0100

    enhance product page layout with footer and improved structure

--- src/app/sklep/page.jsx
+++ src/app/sklep/page.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 
 import { FadeIn, FadeInStagger } from "@/components/FadeIn"
 import { Container } from '@/components/Container'
+import Footer from '@/components/Footer'
 import kuracjaPodstawowa from '@/images/sklep/kuracja_podstawowa.JPG'
 import aparatRuchu from '@/images/sklep/aparat_ruchu.jpg'
 import kuracjaOdpornosciowa from '@/images/sklep/kuracja_odpornosciowa.jpg'
@@ -119,93 +120,96 @@ const products = [
 
 export default function Sklep() {
   return (
-    <Container className="mt-16 sm:mt-32 pb-16 sm:pb-32">
-      <div className="bg-white py-24">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <p className="text-base/7 font-semibold text-lime-600">Sklep</p>
-            <h2 className="mt-2 text-5xl font-medium tracking-tight text-gray-900 sm:text-5xl">Zakupy tylko w programie partnerskim</h2>
-            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
-              aktywacja konta bezpośrednio u producenta, cieszy się zawsze najlepszą ceną i jest jedyną gwarancją oryginalności produktu. 
-            </p>
+    <>
+      <Container className="mt-16 sm:mt-32 pb-16 sm:pb-32">
+        <div className="bg-white py-24">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:mx-0">
+              <p className="text-base/7 font-semibold text-lime-600">Sklep</p>
+              <h2 className="mt-2 text-5xl font-medium tracking-tight text-gray-900 sm:text-5xl">Zakupy tylko w programie partnerskim</h2>
+              <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+                aktywacja konta bezpośrednio u producenta, cieszy się zawsze najlepszą ceną i jest jedyną gwarancją oryginalności produktu. 
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="bg-white">
-        <FadeInStagger className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <FadeIn key={product.id}>
-                <Link key={product.id} href={product.href} className="group">
-                  <Image alt='' src={product.imageSrc} loading="lazy"
-                    className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
-                  />
-                  <p className="mt-4 text-lg font-medium text-gray-900">{product.title}</p>
-                  <h3 className="mt-1 text-sm text-gray-700">{product.description}</h3>
-                </Link>
-              </FadeIn>
-            ))}
-          </div>
-        </FadeInStagger>
-      </div>
-      <FadeIn className="bg-white">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 py-24 sm:py-32 lg:py-40">
-          <div className="relative isolate overflow-hidden bg-gray-800 px-6 pt-16 shadow-2xl rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-            <svg viewBox="0 0 1024 1024" aria-hidden="true" className="absolute left-1/2 top-1/2 -z-10 size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0">
-              <circle r={512} cx={512} cy={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
-              <defs>
-                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                  <stop stopColor="#3f6212" />
-                  <stop offset={1} stopColor="#65a30d" />
-                </radialGradient>
-              </defs>
-            </svg>
-            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-              <h2 className="text-balance text-3xl font-semibold tracking-tight text-white sm:text-4xl">
-                Dołącz do Programu Partnerskiego
-              </h2>
-              <p className="mt-6 text-pretty text-lg/8 text-gray-300">
-                Poniższa witryna zawiera wyselekcjonowane produkty firmy LR Health&Beauty. Pełen katalog produktów, znajduje się w zakładce „Sklep”
-              </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                <Link href="https://shop.lrworld.com/cms/PL/pl/?casrnc=a2409" target='_blank'
-                  className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100
-                  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
-                  Sklep
-                </Link>
-                <span className="text-sm/6 font-semibold text-white">
-                  Znajdź produkty LR Health&Beauty.
-                </span>
+        <div className="bg-white">
+          <FadeInStagger className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <FadeIn key={product.id}>
+                  <Link key={product.id} href={product.href} className="group">
+                    <Image alt='' src={product.imageSrc} loading="lazy"
+                      className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
+                    />
+                    <p className="mt-4 text-lg font-medium text-gray-900">{product.title}</p>
+                    <h3 className="mt-1 text-sm text-gray-700">{product.description}</h3>
+                  </Link>
+                </FadeIn>
+              ))}
+            </div>
+          </FadeInStagger>
+        </div>
+        <FadeIn className="bg-white">
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 py-24 sm:py-32 lg:py-40">
+            <div className="relative isolate overflow-hidden bg-gray-800 px-6 pt-16 shadow-2xl rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+              <svg viewBox="0 0 1024 1024" aria-hidden="true" className="absolute left-1/2 top-1/2 -z-10 size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0">
+                <circle r={512} cx={512} cy={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+                <defs>
+                  <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                    <stop stopColor="#3f6212" />
+                    <stop offset={1} stopColor="#65a30d" />
+                  </radialGradient>
+                </defs>
+              </svg>
+              <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                <h2 className="text-balance text-3xl font-semibold tracking-tight text-white sm:text-4xl">
+                  Dołącz do Programu Partnerskiego
+                </h2>
+                <p className="mt-6 text-pretty text-lg/8 text-gray-300">
+                  Poniższa witryna zawiera wyselekcjonowane produkty firmy LR Health&Beauty. Pełen katalog produktów, znajduje się w zakładce „Sklep”
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                  <Link href="https://shop.lrworld.com/cms/PL/pl/?casrnc=a2409" target='_blank'
+                    className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100
+                    focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+                    Sklep
+                  </Link>
+                  <span className="text-sm/6 font-semibold text-white">
+                    Znajdź produkty LR Health&Beauty.
+                  </span>
+                </div>
+              </div>
+              <div className="relative mt-16 h-80 lg:mt-8">
+                <Image
+                  alt="App screenshot"
+                  src={main}
+                  width={1824}
+                  height={1080}
+                  className="absolute -left-28 -top-3 w-[57rem] max-w-none rounded-md"
+                />
               </div>
             </div>
-            <div className="relative mt-16 h-80 lg:mt-8">
-              <Image
-                alt="App screenshot"
-                src={main}
-                width={1824}
-                height={1080}
-                className="absolute -left-28 -top-3 w-[57rem] max-w-none rounded-md"
-              />
-            </div>
+          </div>
+        </FadeIn>
+        <div className="bg-white">
+          <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+            <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Często zadawane pytania</h2>
+            <dl className="mt-20 divide-y divide-gray-900/10">
+              {faqs.map((faq) => (
+                <FadeInStagger key={faq.id} className="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+                  <dt className="text-base/7 font-semibold text-gray-900 lg:col-span-5">{faq.question}</dt>
+                  <dd className="mt-4 lg:col-span-7 lg:mt-0">
+                    <FadeIn className="text-base/7 text-gray-600">{faq.answer}</FadeIn>
+                  </dd>
+                </FadeInStagger>
+              ))}
+            </dl>
           </div>
         </div>
-      </FadeIn>
-      <div className="bg-white">
-        <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
-          <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Często zadawane pytania</h2>
-          <dl className="mt-20 divide-y divide-gray-900/10">
-            {faqs.map((faq) => (
-              <FadeInStagger key={faq.id} className="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
-                <dt className="text-base/7 font-semibold text-gray-900 lg:col-span-5">{faq.question}</dt>
-                <dd className="mt-4 lg:col-span-7 lg:mt-0">
-                  <FadeIn className="text-base/7 text-gray-600">{faq.answer}</FadeIn>
-                </dd>
-              </FadeInStagger>
-            ))}
-          </dl>
-        </div>
-      </div>
-    </Container>
+      </Container>
+      <Footer />
+    </>
   )
 }
